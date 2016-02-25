ebbd77063569a2da9b8cb863b851b1b8637279af
xuguoliangjj <1044748759@qq.com>
2016-02-25 17:54:49 +0800

    修改filter过滤前端

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -54,26 +54,62 @@ AppAsset::register($this);
                 <div class="row">
                         <div class="panel panel-default own-panel">
                             <div class="panel-body">
-                                <div class="col-xs-12 col-sm-10">
-                                    <input size="16" type="text" value="2012-06-15" readonly class="form_datetime">
-                                    <?= \yii\bootstrap\Button::widget([
-                                        'label' => '<span>2016-01-01 - 2016-01-05</span> 日期&nbsp;&nbsp;<i class="fa fa-calendar"></i>',
-                                        'options'=>['id'=>'own-date-filter','class'=>'btn btn-success'],
-                                        'encodeLabel'=>false
-                                    ])?>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-10">
+                                        <?= \yii\bootstrap\Button::widget([
+                                            'label' => '<span id="own-date-start">2016-01-01</span> - <span id="own-date-end">2016-01-05</span>&nbsp;&nbsp;<i class="fa fa-calendar"></i>',
+                                            'options'=>['id'=>'own-date-filter','class'=>'btn btn-success'],
+                                            'encodeLabel'=>false
+                                        ])?>
+                                        <div class="btn-group" role="group" aria-label="...">
+                                            <button type="button" class="btn btn-default">今天</button>
+                                            <button type="button" class="btn btn-default">昨天</button>
+                                            <button type="button" class="btn btn-default">近7天</button>
+                                            <button type="button" class="btn btn-default">近15天</button>
+                                            <button type="button" class="btn btn-default">近30天</button>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-2">
+                                        <?php
+                                        \yii\bootstrap\Modal::begin([
+                                            'options'=>['class'=>'own-filter'],
+                                            'header' => '<h5>筛选</h5>',
+                                            'footer' => '<button type="button" class="btn btn-primary" data-dismiss="modal">取消</button>
+                                                         <button type="button" class="btn btn-primary" id="own-sure-filter">确认</button>',
+                                            'toggleButton' => ['label' => '<i class="fa fa-filter"></i>&nbsp;&nbsp;过滤','class'=>'btn btn-success btn-sm pull-right'],
+                                        ]);
+                                        ?>
+                                        aaaaaaaa
+                                        <?php
+                                        \yii\bootstrap\Modal::end();
+                                        ?>
+                                    </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-2">
-                                    <?php
-                                    \yii\bootstrap\Modal::begin([
-                                        'options'=>['class'=>'own-filter'],
-                                        'header' => '<h5>筛选</h5>',
-                                        'footer' => '<button type="button" class="btn btn-primary" data-dismiss="modal">取消</button>
-                                                     <button type="button" class="btn btn-primary" id="own-sure-filter">确认</button>',
-                                        'toggleButton' => ['label' => '<i class="fa fa-filter"></i>&nbsp;&nbsp;过滤','class'=>'btn btn-success btn-sm pull-right'],
-                                    ]);
-                                    echo 'Say hello...';
-                                    \yii\bootstrap\Modal::end();
-                                    ?>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12">
+                                        <div id="own-platform">
+                                            <span class="own-title">平 台：</span>
+                                            <span class="label label-default">中国&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                            <span class="label label-default">台湾&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                            <span class="label label-default">日本&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                        </div>
+                                        <br>
+                                        <div id="own-server">
+                                            <span class="own-title">区 服：</span>
+                                            <span class="label label-default">安卓1服&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                            <span class="label label-default">安卓2服&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                            <span class="label label-default">安卓3服&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                            <span class="label label-default">安卓4服&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+
+                                        </div>
+                                        <br>
+                                        <div id="own-channel">
+                                            <span class="own-title">渠 道：</span>
+                                            <span class="label label-default">安卓&nbsp;<i class="fa fa-close own-close-filter-label"></i></span>
+                                        </div>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- backend/web/js/main.js
+++ backend/web/js/main.js
@@ -47,7 +47,52 @@ $(function(){
         $("#"+id).modal('hide');
     });
     $('[data-toggle="popover"]').popover();
-    $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd'});
+
+    //日期选择
+    var datetimepickerOptions = {
+        minView: "month", //选择日期后，不会再跳转去选择时分秒
+        format: "yyyy-mm-dd", //选择日期后，文本框显示的日期格式
+        autoclose:true, //选择日期后自动关闭
+        bootcssVer:3,
+        pickerPosition: "bottom-left",
+        linkField:"own-date-start"
+    };
+    $("#own-date-filter").click(function(e){
+        $("#own-date-start").datetimepicker(datetimepickerOptions).on('changeDate', function(ev){
+            $("#own-date-start").empty().text(ev.date.Format("yyyy-MM-dd"));
+            $("#own-date-end").datetimepicker(datetimepickerOptions).on('changeDate', function(ev){
+                $(this).empty().text(ev.date.Format("yyyy-MM-dd"));
+            });
+            $("#own-date-end").datetimepicker("show");
+        })
+        $("#own-date-start").datetimepicker("show");
+    });
+
+    //删除过滤条件
+    $(".own-close-filter-label").click(function(){
+        if($(this).parent().siblings('span').length >= 2) {
+            $(this).parent().remove();
+        }else{
+            var filter = $(this).parent().parent();
+            $(this).parent().remove();
+            filter.fadeOut();
+        }
+    });
 });
 
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
 
