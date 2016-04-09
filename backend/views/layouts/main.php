7e225e1a5bbb7b1c98cb31ff751f2fa0496979fd
xuguoliangjj <1044748759@qq.com>
2016-04-09 22:19:13 +0800

    change

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -20,45 +20,40 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-<div id="own-header">
+<div class="header">
     <?= $this -> context -> renderPartial('@backend/views/layouts/_top');?>
 </div>
-<div class="wrap">
-    <div class="container-fluid own-container-fluid">
-        <?php if(!Yii::$app->user->isGuest && !empty($this ->context -> leftMenu)):?>
-        <div class="row own-main">
-            <div class="col-xs-12 col-sm-2 own-left-bar">
-                <div class="own-search-bar hidden-xs">
-                    <div class="input-group input-group" style="padding:10px;">
-                        <input type="text" class="form-control" placeholder="搜索......" aria-describedby="sizing-addon1">
-                        <span class="input-group-addon btn" id="sizing-addon1"><span class="glyphicon glyphicon-search"></span></span>
-                    </div>
+<div class="content">
+    <div class="row" id="own-main">
+        <div class="col-xs-12 col-sm-2 own-left-bar">
+            <div class="own-search-bar hidden-xs">
+                <div class="input-group input-group" style="padding:10px;">
+                    <input type="text" class="form-control" placeholder="搜索......" aria-describedby="sizing-addon1">
+                    <span class="input-group-addon btn" id="sizing-addon1"><span class="glyphicon glyphicon-search"></span></span>
                 </div>
-                    <?php if (!Yii::$app->user->isGuest):?>
-                        <?=$this -> context -> renderPartial('@backend/views/layouts/_left');?>
-                    <?php endif;?>
-
             </div>
-            <?php endif;?>
-            <div class="col-xs-12 col-sm-10 own-main-bar">
-                <?= Breadcrumbs::widget([
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                    'options' => ['class' => 'breadcrumb'],
-                    'homeLink' => [
-                        'label' => '首页',                  // required
-                        'url' => '/',                      // optional, will be processed by Url::to()
-                        'template' => "<li>{link}</li>\n", // optional, if not set $this->itemTemplate will be used
-                    ]
-                ]) ?>
-                <div class="row">
-                    <?= $content ?>
-                </div>
+                <?php if (!Yii::$app->user->isGuest):?>
+                    <?=$this -> context -> renderPartial('@backend/views/layouts/_left');?>
+                <?php endif;?>
+        </div>
+        <div class="col-xs-12 col-sm-10 own-main-bar">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                'options' => ['class' => 'breadcrumb'],
+                'homeLink' => [
+                    'label' => '首页',                  // required
+                    'url' => '/',                      // optional, will be processed by Url::to()
+                    'template' => "<li>{link}</li>\n", // optional, if not set $this->itemTemplate will be used
+                ]
+            ]) ?>
+            <div class="row">
+                <?= $content ?>
             </div>
         </div>
+        <div class="clear:both"></div>
     </div>
 </div>
 
-<div style="clear: both;"></div>
 <div class="footer">
     <div class="container">
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
