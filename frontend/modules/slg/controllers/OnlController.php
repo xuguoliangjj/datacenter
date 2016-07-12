eb0b40f902cf21e8cdc36345bd39f8cf55704363
xuguoliang <xuguoliang@xuguoliangdeMBP.lan>
2016-07-12 23:22:13 +0800

    change db

---
+++ frontend/modules/slg/controllers/OnlController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 16/7/12
+ * Time: 22:56
+ * @author: xuguoliang <1044748759@qq.com>
+ */
+namespace frontend\modules\slg\controllers;
+
+use Yii;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\RateLimiter;
+use yii\filters\VerbFilter;
+use yii\rest\ActiveController;
+use yii\web\Response;
+
+class OnlminController extends ActiveController
+{
+    public $modelClass = 'frontend\modules\slg\models\Avp';
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::className(),
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                    'application/xml' => Response::FORMAT_XML,
+                ],
+            ],
+            'verbFilter' => [
+                'class' => VerbFilter::className(),
+                'actions' => $this->verbs(),
+            ],
+            'authenticator' => [
+                'class' => CompositeAuth::className(),
+            ],
+            'rateLimiter' => [
+                'class' => RateLimiter::className(),
+            ]
+        ];
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'index' => [
+                'class' => 'restAction\avp\IndexAction',
+                'modelClass'  => $this->modelClass,
+                'checkAccess' => [$this, 'checkAccess'],
+            ],
+            'view' => [
+                'class' => 'restAction\avp\ViewAction',
+                'modelClass'  => $this->modelClass,
+                'checkAccess' => [$this, 'checkAccess'],
+            ],
+            'options' => [
+                'class' => 'yii\rest\OptionsAction',
+            ],
+        ];
+    }
+}
